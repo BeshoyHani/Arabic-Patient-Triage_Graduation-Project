7a1e19e982f100c0b576bed9297738bebc3d758e
BeshoyHani <be4oy.hani@gmail.com>
2022-04-14 14:51:32 +0200

    Taking Voice as Input and Convert it to String

---
+++ Taking Voice As Input/VoiceRecognition.py
@@ -0,0 +1,43 @@
+import os
+
+import numpy as np
+from google.cloud import speech
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'idyllic-striker-341412-40c520a7335a.json'
+speech_client = speech.SpeechClient()
+
+# Example 1 & 2. Transcribe Local Media File
+# File Size: < 10mbs, length < 1 minute
+
+
+def getText(content):
+    """
+     Note that transcription is limited to a 60 seconds audio file.
+     Use a GCS file for audio longer than 1 minute.
+    """
+    audio = speech.RecognitionAudio(content=content)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=48000,
+        language_code="ar-EG",
+        # audio_channel_count=2,
+    )
+
+    operation = speech_client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
+
+    text = ""
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        text += result.alternatives[0].transcript
+        # The first alternative is the most likely one for this portion.
+        # print(u"Transcript: {}".format(result.alternatives[0].transcript))
+        # print("Confidence: {}".format(result.alternatives[0].confidence))
+
+    return text
+
+#print(getText('doc.wav'))
---
+++ Taking Voice As Input/main.py
@@ -0,0 +1,60 @@
+'''
+import pandas as pd
+import csv
+df = pd.read_csv('Training.csv')
+print(df['prognosis'].unique())
+y = df['prognosis'].unique()
+pd.DataFrame(y).to_csv('prognosis.csv')
+'''
+import os
+from tkinter import *
+from PIL import Image, ImageTk
+import speech_recognition as sr
+import VoiceRecognition as VC
+
+
+class Main:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Patient Triage")
+        self.window.geometry("300x400")
+        self.window.eval('tk::PlaceWindow . center')
+
+        # Load Icon for record button
+        self.file = './assets/images/mic2.ico'
+        self.image = Image.open(self.file)
+        self.mic_icon = ImageTk.PhotoImage(self.image.resize((100, 100)))
+
+        # Label for Record Button
+        self.btn_lbl = StringVar()
+        self.btn_lbl.set("Click to Record")
+        Label(textvariable=self.btn_lbl).pack(anchor=CENTER, pady=(100, 20))
+
+        self.sound_btn = Button(self.window, image=self.mic_icon, width=64, height=64, relief=FLAT, command=self.record_audio)
+        self.sound_btn["border"] = "0"
+        self.sound_btn.pack(anchor=CENTER)
+
+        self.window.mainloop()
+
+    def record_audio(self):
+        # Update the Record Button Label
+        self.btn_lbl.set("Recording...")
+        self.window.update()
+
+        # Record Voice
+        mic = sr.Microphone(device_index=1)
+        voice_recognizer = sr.Recognizer()
+
+        with mic as source:
+            print("Say Hi")
+            voice_recognizer.adjust_for_ambient_noise(source)
+            audio = voice_recognizer.listen(source)
+
+        print(voice_recognizer.recognize_google(audio ,language="ar-EG"))
+        #print(VC.getText(audio))
+
+        # Update Record Button Label
+        self.btn_lbl.set("Click to Record")
+
+
+main = Main()
