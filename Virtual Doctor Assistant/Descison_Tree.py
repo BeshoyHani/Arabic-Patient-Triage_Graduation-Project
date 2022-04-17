560f45630e2e9169c4e601979c602c28dad425eb
IsaacMoris <isaac.moris9@gmail.com>
2022-04-18 00:04:24 +0200

    model

--- Virtual Doctor Assistant/Descison_Tree.py
+++ Virtual Doctor Assistant/Descison_Tree.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier, Ran
 from sklearn.svm import SVC
 
 df = pd.read_csv('kaggle/Training.csv')
-df_test = pd.read_csv("kaggle/Testing.csv")
+#df_test = pd.read_csv("kaggle/Testing.csv")
 #
 # df.describe()
 
@@ -24,15 +24,14 @@ from sklearn.model_selection import train_test_split
 
 x = df.drop('prognosis', axis=1)
 y = df['prognosis']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=True, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.6, shuffle=True)
 
 
 def Decision_Tree():
     tree = DecisionTreeClassifier()
     tree.fit(x_train, y_train)
 
-    pred = tree.predict(x_test)
-    acc = tree.score(x_test, pred)
+    acc = tree.score(x_test, y_test)
 
     print("Decision Tree Acurray on test set: {:.2f}%".format(acc * 100))
     # print(tree.get_depth())
@@ -42,7 +41,6 @@ def Decision_Tree():
 def Random_Forest():
     clf = RandomForestClassifier()
     clf.fit(x_train, y_train)
-    predforest = clf.predict(x_test)
     accForest = clf.score(x_test, y_test)
     print("Random ForestAcurray on test set: {:.2f}%".format(accForest * 100))
     return clf
@@ -62,12 +60,12 @@ def GBC():
 #######################################
 
 
-def modelling(df1):
-    x = df1.drop('prognosis', axis=1).values
-    y = df1.prognosis
+def modelling():
+    #x = df1.drop('prognosis', axis=1).values
+    #y = df1.prognosis
     #x_train_new, x_test_new, y_train_new, y_test_new = train_test_split(x_new, y_new, test_size=0.3, random_state=42)
-    tree = Decision_Tree()
-    tree.fit(x, y)
+    tree = Random_Forest()
+    #tree.fit(x, y)
     return tree
 #     pred_new = tree.predict(x_test_new)
 #
@@ -77,9 +75,7 @@ def modelling(df1):
 # #   print("mean_absolute_error of the test set: {:.2f}%".format(a))
 
 
-test = pd.read_csv('kaggle/Testing.csv')
-model = modelling(test)
-
+model = modelling()
 
 def testing(symptoms):
     newTest = np.zeros((1, 132))
